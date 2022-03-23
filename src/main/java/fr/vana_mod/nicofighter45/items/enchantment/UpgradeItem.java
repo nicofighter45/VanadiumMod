56f551813c5c880619646c7eda741905f5ae6df6
nicofighter45 <fagot.nicolas.45@gmail.com>
2022-03-23 15:56:54 +0100

    1.18 non working update

--- src/main/java/fr/vana_mod/nicofighter45/items/enchantment/UpgradeItem.java
+++
@@ -1,9 +0,0 @@
-package fr.vana_mod.nicofighter45.items.enchantment;
-
-import net.minecraft.item.Item;
-
-public class UpgradeItem extends Item {
-    public UpgradeItem(Settings settings) {
-        super(settings);
-    }
-}
